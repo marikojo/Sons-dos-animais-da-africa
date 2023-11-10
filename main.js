3a066fea39369f11700641d26235ca429405c2be
Mariane kojo <104777124+marikojo@users.noreply.github.com>
2023-11-10 16:30:13 -0300

    56w5us865

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+   document.querySelector('.tecla_Elefante').play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_Elefante').onclick = tocaSomElefante;
 
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
