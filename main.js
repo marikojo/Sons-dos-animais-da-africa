f5d95e1054cdf9a47d42e4cef68791e52ce91284
Mariane kojo <104777124+marikojo@users.noreply.github.com>
2023-11-22 17:53:24 -0300

    duk

--- main.js
+++ main.js
@@ -14,3 +14,20 @@ function tocaSomCrocodilo() {
     document.querySelector('#som_tecla_crocodilo').play();
 }
 document.querySelector('tecla_crocodilo').onclick = tocaSomGorila
+
+function tocaSomBúfalo() {
+    document.querySelector('#som_tecla_búfalo').play();
+}
+document.querySelector('tecla_búfalo').onclick = tocaSomBúfala;
+
+
+function tocaSomLeopardo() {
+    document.querySelector('#som_tecla_leopardo').play();
+}
+document.querySelector('tecla_leopardo').onclick = tocaSomLeopardo;
+
+
+function tocaSomLeão() {
+    document.querySelector('#som_tecla_leão').play();
+}
+document.querySelector('tecla_leão').onclick = tocaSomLeão;
